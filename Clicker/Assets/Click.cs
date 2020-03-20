2187f54e837b1f38a49779f164c3be62b694808d
erdosten <olegsan59@gmail.com>
2020-03-20 20:09:40 +0300

    Сделал как в задании. Есть проблема: все блоки меняю свой размер, даже те которые еще не заспаунились.

---
+++ Clicker/Assets/Click.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Click : MonoBehaviour
+{
+    public int health;
+    public float time;
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        time -= Time.deltaTime;
+        Debug.Log("Time" + time);
+        if (health > 0 && time > 0)
+        {
+            transform.localScale += new Vector3(0.4f, 0.4f, 0.4f) * Time.deltaTime;
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnMouseDown()
+    {
+        health--;
+    }
+}
---
+++ Clicker/Assets/Spawner.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject CubePrefab;
+
+    public float SpawnTime;
+    void Start()
+    {
+        InvokeRepeating("SpawnCube", 0, SpawnTime);
+    }
+
+    
+    void SpawnCube()
+    {
+        GameObject cobe = Instantiate(CubePrefab, transform.position, transform.rotation) as GameObject;
+    }
+}
